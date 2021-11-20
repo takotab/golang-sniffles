cafa944dad74efd859fbeb961539ea6487838620
Tako CarbonX <takotabak@gmail.com>
2021-11-20 21:31:32 +0100

    improving it a tiny bit with a channel

--- read_excel.go
+++ read_excel.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+	"sync"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
 	// "github.com/go-gota/gota/dataframe"
@@ -20,9 +21,9 @@ type Row struct {
 	values []string
 }
 
-func main() {
-	write()
-	f, err := excelize.OpenFile("simple.xlsx")
+func get_max_rows(fn string, column_numbers []int, return_chan chan []string) {
+
+	f, err := excelize.OpenFile(fn)
 
 	if err != nil {
 		log.Fatal(err)
@@ -34,8 +35,9 @@ func main() {
 		return
 	}
 	ColumnsToGetMax := make(map[int]Row)
-	ColumnsToGetMax[1] = Row{0, []string{}}
-	ColumnsToGetMax[2] = Row{0, []string{}}
+	for _, column_num := range column_numbers {
+		ColumnsToGetMax[column_num] = Row{}
+	}
 	for _, row := range rows {
 		for col_index, colCell := range row {
 			if CurrentMaxRow, ok := ColumnsToGetMax[col_index]; ok {
@@ -51,5 +53,24 @@ func main() {
 		fmt.Println(row)
 	}
 	fmt.Println(ColumnsToGetMax)
+	for _, columnMax := range ColumnsToGetMax {
+		return_chan <- columnMax.values
+	}
+}
+
+func main() {
+	write()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	maxRows := make(chan []string, 100)
+	go func() {
+		get_max_rows("simple.xlsx", []int{1, 2}, maxRows)
+		wg.Done()
+	}()
+	for i := 0; i < 2; i++ {
+		fmt.Println(<-maxRows)
+	}
+
+	wg.Wait()
 
 }
