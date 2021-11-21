51c7feb36bb1405f78b559c316d63674f483b51d
Tako CarbonX <takotabak@gmail.com>
2021-11-21 09:00:11 +0100

    split it into multiple files

--- read_excel.go
+++ read_excel.go
@@ -2,61 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 	"sync"
-
-	"github.com/360EntSecGroup-Skylar/excelize/v2"
 	// "github.com/go-gota/gota/dataframe"
 )
 
 func check(e error) {
 	if e != nil {
 		panic(e)
-	}
-}
-
-type Row struct {
-	max    float64
-	values []string
-}
-
-func get_max_rows(fn string, column_numbers []int, return_chan chan []string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	f, err := excelize.OpenFile(fn)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Get all the rows in the Sheet1.
-	rows, err := f.GetRows("Sheet1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	ColumnsToGetMax := make(map[int]Row)
-	for _, column_num := range column_numbers {
-		ColumnsToGetMax[column_num] = Row{}
-	}
-	for _, row := range rows {
-		for col_index, colCell := range row {
-			if CurrentMaxRow, ok := ColumnsToGetMax[col_index]; ok {
-				if s, err := strconv.ParseFloat(colCell, 32); err == nil {
-					if CurrentMaxRow.max < s {
-						ColumnsToGetMax[col_index] = Row{s, row}
-						// fmt.Println("high", col_index, s)
-					}
-				}
-
-			}
-		}
-		fmt.Println(row)
-	}
-	fmt.Println(ColumnsToGetMax)
-	for _, columnMax := range ColumnsToGetMax {
-		return_chan <- columnMax.values
 	}
 }
 
@@ -70,7 +24,7 @@ func main() {
 	wg.Wait()
 
 	files_found := make(chan string, 1000)
-	err := filepath.Walk(".",
+	check(filepath.Walk(".",
 		func(path string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
@@ -80,10 +34,7 @@ func main() {
 				files_found <- path
 			}
 			return nil
-		})
-	if err != nil {
-		log.Println(err)
-	}
+		}))
 	close(files_found)
 	fmt.Println("done")
 
--- simple.go
+++ simple.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 	"sync"
 
 	"github.com/360EntSecGroup-Skylar/excelize/v2"
@@ -32,5 +34,47 @@ func write(fn string, wg *sync.WaitGroup) {
 
 	if err := f.SaveAs(fn); err != nil {
 		log.Fatal(err)
+	}
+}
+
+type Row struct {
+	max    float64
+	values []string
+}
+
+func get_max_rows(fn string, column_numbers []int, return_chan chan []string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	f, err := excelize.OpenFile(fn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Get all the rows in the Sheet1.
+	rows, err := f.GetRows("Sheet1")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	ColumnsToGetMax := make(map[int]Row)
+	for _, column_num := range column_numbers {
+		ColumnsToGetMax[column_num] = Row{}
+	}
+	for _, row := range rows {
+		for col_index, colCell := range row {
+			if CurrentMaxRow, ok := ColumnsToGetMax[col_index]; ok {
+				if s, err := strconv.ParseFloat(colCell, 32); err == nil {
+					if CurrentMaxRow.max < s {
+						ColumnsToGetMax[col_index] = Row{s, row}
+						// fmt.Println("high", col_index, s)
+					}
+				}
+
+			}
+		}
+		fmt.Println(row)
+	}
+	fmt.Println(ColumnsToGetMax)
+	for _, columnMax := range ColumnsToGetMax {
+		return_chan <- columnMax.values
 	}
 }
